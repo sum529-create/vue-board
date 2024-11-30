88afecb2978eb28115e81a4e2ea15fa09574f579
노수민 <>
2024-11-30 18:46:58 +0900

    init setting

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeader from "./layouts/TheHeader.vue";
+import TheView from "./layouts/TheView.vue";
+</script>
+
+<template>
+  <the-header />
+  <the-view />
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");

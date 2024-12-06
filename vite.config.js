51fb7722024b8bc887c16b8c04a0435707bc6bfb
노수민 <nosumin29@gmail.com>
2024-12-06 12:34:53 +0900

    fix: unplugin-vue-components 적용

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-import Components from "unplugin-vue-components";
+import Components from "unplugin-vue-components/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
